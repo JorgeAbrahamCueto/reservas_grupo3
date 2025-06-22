eb716021ddf4a6da284088b56f74fbd73a398a34
JorgeAbrahamCueto <A19106487@idat.pe>
2025-06-21 22:50:59 -0500

    primer cambio en el menu en dispositivos moviles

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -4,59 +4,70 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
 
 export const Navegacion = () => {
+  // Estado para controlar si el menú está abierto o cerrado
+  const [menuAbierto, setMenuAbierto] = React.useState(false);
+
+  // Función para alternar el menú
+  const toggleMenu = () => setMenuAbierto((prev) => !prev);
+
+  // Cierra el menú al hacer clic en un enlace
+  const handleNavLinkClick = () => setMenuAbierto(false);
+
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom-black">
-      <div className="container-fluid mx-3">
-        
-        {/* --- LOGO REESTRUCTURADO --- */}
-        <NavLink className="navbar-brand" to="/">
-          <div className="d-flex flex-column align-items-center">
-            {/* Usamos la ruta que mencionaste: public/img/logo.png */}
-            <img 
-              src="/img/logo.png" 
-              alt="Logo" 
-              style={{height: '100px' }} // Ajustamos un poco la altura
-            />
+      <nav className="navbar navbar-expand-lg navbar-dark navbar-custom-black">
+        <div className="container-fluid mx-3">
+          {/* --- LOGO REESTRUCTURADO --- */}
+          <NavLink className="navbar-brand" to="/">
+            <div className="d-flex flex-column align-items-center">
+              <img 
+                src="/img/logo.png" 
+                alt="Logo" 
+                style={{height: '100px' }}
+              />
+            </div>
+          </NavLink>
+
+          {/* Botón para menú en móviles */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            aria-controls="navbarNav"
+            aria-expanded={menuAbierto}
+            aria-label="Toggle navigation"
+            onClick={toggleMenu}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* --- LINKS DE NAVEGACIÓN --- */}
+          <div
+            className={`collapse navbar-collapse justify-content-end${menuAbierto ? ' show' : ''}`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/" onClick={handleNavLinkClick}>Inicio</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/nosotros" onClick={handleNavLinkClick}>Nosotros</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/carta" onClick={handleNavLinkClick}>Carta</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contactos" onClick={handleNavLinkClick}>Contactos</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/reservas" onClick={handleNavLinkClick}>Reservas</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/reservasclientes" onClick={handleNavLinkClick}>ReservasCliente</NavLink>
+              </li>
+            </ul>
           </div>
-        </NavLink>
-
-        {/* Botón para menú en móviles */}
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        {/* --- LINKS DE NAVEGACIÓN --- */}
-        {/* Quitamos fw-bold de cada link */}
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/">Inicio</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/nosotros">Nosotros</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/carta">Carta</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contactos">Contactos</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/reservas">Reservas</NavLink>
-            </li>
-             <li className="nav-item">
-              <NavLink className="nav-link" to="/reservasclientes">ReservasCliente</NavLink>
-            </li>
-          </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
-  )
+  );
 }
