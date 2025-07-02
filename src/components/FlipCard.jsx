0d9de9c56fa4ce39edee742d0fa59f5373b5975a
JorgeAbrahamCueto <A19106487@idat.pe>
2025-07-02 09:28:39 -0500

    Actualizar FlipCard.jsx

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './FlipCard.css'; // Asegúrate de crear este archivo para los estilos
+import './FlipCard.css';
 
 const FlipCard = ({ src, alt, text }) => {
   return (
